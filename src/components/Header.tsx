60cbffe89be29e57988fbc3f2e04c91a30c4706c
Elix Neto <neto.elix@outlook.com>
2023-11-08 02:40:48 -0300

    Criando header do componente wallet, req 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+
+type GlobalState = {
+  user: {
+    email: string;
+  };
+};
+
 function Header() {
-  return <div>Header</div>;
+  const email = useSelector((state: GlobalState) => state.user.email);
+
+  return (
+    <div>
+      <h1 data-testid="email-field">{email}</h1>
+      <ol data-testid="total-field">
+        <li>0</li>
+      </ol>
+      <ol data-testid="header-currency-field">
+        <li>BRL</li>
+      </ol>
+    </div>
+  );
 }
 
 export default Header;
