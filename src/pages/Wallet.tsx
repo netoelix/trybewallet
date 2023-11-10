cab88b271654be1a75bfefd3b886477574ea3656
Elix Neto <neto.elix@outlook.com>
2023-11-10 04:20:22 -0300

    Finalizando requisito 5

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 function Wallet() {
+  const [expenses, setExpenses] = React.useState([]);
+
   return (
     <div>
       <Header />
-      <WalletForm />
+      <WalletForm setExpenses={ setExpenses } />
+      <Table expenses={ expenses } />
     </div>
   );
 }
